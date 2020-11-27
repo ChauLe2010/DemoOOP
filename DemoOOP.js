c0a9ac1b747f5b2d95e548ca4561f69d272f45bc
chau.le <chau.le@codegym.vn>
2020-11-27 09:10:44 +0700

    oop

--- DemoOOP.js
+++ DemoOOP.js
@@ -2,6 +2,25 @@ function Car(name,color,model){
     this.name=name;
     this.color=color;
     this.model=model;
+    this.getName=function(){
+        return this.name;
+    }
+    this.getColor=function () {
+        return this.color;
+    }
+    this.getModel=function () {
+        return this.model;
+    }
+    this.setName=function (name) {
+        this.name=name;
+    }
+    this.setColor=function (color) {
+        this.color=color;
+    }
+    this.setModel=function (model) {
+        this.model=model;
+    }
+
     this.start=function(){
         document.write("<br>Khoi dong xe");
     }
@@ -14,10 +33,8 @@ function Car(name,color,model){
 }
 let carFer=new Car("Nguyen","blue","Fer");
 let carBMW=new Car("Quan","pink","BMW");
-carFer.display();
-carBMW.display();
-carFer.start();
-carBMW.start();
-carFer.stop();
-document.write("<br>"+carFer.name);
 
+document.write("<br>"+carFer.name);
+//carFer.name="Audi";
+carFer.setName("Audi");
+document.write("<br>"+carFer.getName());
---
+++ Rectangle.js
@@ -0,0 +1,17 @@
+function Rectangle(width,height) {
+    this.width=width;
+    this.height=height;
+
+    this.area=function () {
+        return this.width*this.height;
+      // return width*height;
+
+    }
+    this.perimeter=function () {
+        return (this.width+this.height)*2;
+    }
+    this.draw=function () {
+        document.write("<br>Hinh chu nhat co chieu dai: "+this.width+" chieu rong: "+this.height);
+    }
+
+}
